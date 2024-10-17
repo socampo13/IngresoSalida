db2eaa8200791943ec26134f45cfe347520f43dd
socampo13 <montioca16@gmail.com>
2024-10-17 08:39:19 -0500

    Agregar limitantes

--- public/buttons.js
+++ public/buttons.js
@@ -1,6 +1,18 @@
+// Validación de longitud de identificación
+function validarIdentificacion(identificacion){
+  const regex = /^[0-9]{8,10}$/;
+  return regex.test(identificacion);
+}
+
+// Registrar ingreso
 document.getElementById('ingreso-btn').addEventListener('click', () => {
     const identificacion = document.getElementById('identificacion').value;
     
+    if (!validarIdentificacion(identificacion)){
+      document.getElementById('mensaje').textContent = 'Ingresa un numero de identificacion valido';
+      return;
+    }
+
     fetch('/api/registro/ingreso', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -10,6 +22,9 @@ document.getElementById('ingreso-btn').addEventListener('click', () => {
     .then(data => {
         document.getElementById('mensaje').textContent = data.message || data.error;
         document.getElementById('tiempoTranscurrido').textContent = '';
+
+        // Limpiar el input luego de hacer click
+        document.getElementById('identificacion').value = '';
     })
     .catch(error => {
         console.error('Error:', error);
@@ -17,8 +32,14 @@ document.getElementById('ingreso-btn').addEventListener('click', () => {
     });
 });
 
+//Registrar salida
 document.getElementById('salida-btn').addEventListener('click', () => {
     const identificacion = document.getElementById('identificacion').value;
+    // Validar cantidad de numeros
+    if (!validarIdentificacion(identificacion)){
+      document.getElementById('mensaje').textContent = 'Ingresa un numero de identificacion valido';
+      return;
+    }
             
     fetch('/api/registro/salida', {
       method: 'POST',
@@ -35,6 +56,9 @@ document.getElementById('salida-btn').addEventListener('click', () => {
       } else {
         document.getElementById('tiempoTranscurrido').textContent = ''; // Limpiar si no hay tiempo
       }
+
+      // Limpiar el campo de texto de identificacion 
+      document.getElementById('identificacion').value = '';
     })
     .catch(error => {
       console.error('Error:', error);
